9a6fd8daefeac118a49f6c088e8694a0b42ee268
Davis Mazurs <davis.mazurs@knowledgeprice.com>
2020-11-27 17:00:36 +0200

    Initial commit

---
+++ src/main/java/hw/proofit/policycalculator/PolicycalculatorApplication.java
@@ -0,0 +1,13 @@
+package hw.proofit.policycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicycalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicycalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hw/proofit/policycalculator/PolicycalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package hw.proofit.policycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicycalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
